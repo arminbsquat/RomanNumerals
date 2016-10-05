0ff32d2aaa23a9a74202f8220a4b159608b78aba
aberbero <aberbero@STUPID-PC408.stnet.local>
2016-10-05 17:09:30 +0300

    project

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -2,7 +2,111 @@
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
 		// To be Implemented
+		int counter = 0;
+		char[] romanNumCharArray = romanNum.toCharArray();
+		
+			if(romanNumCharArray.length == 1) {
+				counter = singleLengthConverter(romanNumCharArray[0]);
+			}	
+			
+			if(romanNumCharArray.length == 2) {
+				counter = twoLengthConverter(romanNumCharArray[0],romanNumCharArray[1]);
+			}
+			
+			if(romanNumCharArray.length == 3 ) {
+				counter = threeLengthConverter(romanNumCharArray[0],romanNumCharArray[1], romanNumCharArray[2]);
+			}
+			
+			if(romanNumCharArray.length == 4 ) {
+				counter = fourLengthConverter(romanNumCharArray[0],romanNumCharArray[1], romanNumCharArray[2],romanNumCharArray[3]);
+			}
+			
+			
+		
+		
+		
+		return counter;
+
+		
+	}
+	
+
+
+
+	private int simpleConvert(char convertThisChar) {
+		switch (convertThisChar){
+		case 'I': return 1;
+		case 'V': return 5;
+		case 'X': return 10;
+		case 'L': return 50;
+		case 'C': return 100;
+		case 'D': return 500;
+		case 'M': return 1000;
+		default: return 0;
+		}		
+	}
+	
+	private int singleLengthConverter(char convertThisChar) {
+		return this.simpleConvert(convertThisChar);
+	}
+	
+	private int twoLengthConverter(char firstChar, char secondChar) {
+		
+		if(this.simpleConvert(firstChar) >= this.simpleConvert(secondChar)) {
+			return this.simpleConvert(firstChar) + this.simpleConvert(secondChar);
+		} else {
+			return this.simpleConvert(secondChar) - this.simpleConvert(firstChar);
+		}
+		
+		/*
+		if(firstChar == 'I') {
+			if(secondChar == 'I')
+			{
+				return simpleConvert(firstChar) + simpleConvert(secondChar) ;
+			}
+			if(secondChar == 'V')
+			{
+				return simpleConvert(secondChar) - simpleConvert(firstChar);
+			}	
+			if(secondChar == 'X')
+			{
+				return simpleConvert(secondChar) - simpleConvert(firstChar);
+			}
+		}
+
+		if(firstChar == 'V' && secondChar == 'I') {
+			return  simpleConvert(firstChar) + simpleConvert(secondChar);
+		}
+		
+		if(firstChar == 'X' && secondChar == 'I') {
+			return simpleConvert(firstChar) + simpleConvert(secondChar);
+		}
 		return 0;
 		
+		*/
+	}
+	
+	private int threeLengthConverter(char firstChar, char secondChar, char thirdChar) {
+		return simpleConvert(firstChar) + simpleConvert(secondChar) + simpleConvert(thirdChar);
+		
+		
+		/* worked until 11 
+		 * if(firstChar == 'I' && secondChar == 'I' && thirdChar == 'I') {
+			return simpleConvert(firstChar) + simpleConvert(secondChar) + simpleConvert(thirdChar);
+		}
+		if(firstChar == 'V' && secondChar == 'I' && thirdChar == 'I') {
+			return simpleConvert(firstChar) + simpleConvert(secondChar) + simpleConvert(thirdChar);
+		}
+		if(firstChar == 'X' && secondChar == 'I' && thirdChar == 'I') {
+			return simpleConvert(firstChar) + simpleConvert(secondChar) + simpleConvert(thirdChar);
+		}*/
+
+	}
+	
+	private int fourLengthConverter(char firstChar, char secondChar, char thirdChar, char fourChar) {
+		if(firstChar == 'V' && secondChar == 'I' && thirdChar == 'I' &&  fourChar == 'I') {
+			return simpleConvert(firstChar) + simpleConvert(secondChar) + simpleConvert(thirdChar) + simpleConvert(fourChar);
+		}
+		return 0;
 	}
 }
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -3,10 +3,72 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestRomanNumerals {
-
+	RomanNumerals romNum = new RomanNumerals();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConvertToInt_StringNotEmpty() {
+		
+		assertEquals(0, romNum.convertToInteger(""));
+	}
+	
+	@Test public void testConvertToInt_Convert1toRoman() {
+		
+		assertEquals(1, romNum.convertToInteger("I"));
+	}
+	
+	@Test public void testConvertToInt_Convert2toRoman() {
+		
+		assertEquals(2, romNum.convertToInteger("II"));
+	}
+	
+	@Test public void testConvertToInt_Convert3toRoman() {
+		
+		assertEquals(3, romNum.convertToInteger("III"));
+	}
+	
+	@Test public void testConvertToInt_Convert4toRoman() {
+		
+		assertEquals(4, romNum.convertToInteger("IV"));
+	}
+	
+	@Test public void testConvertToInt_Convert5toRoman() {
+		
+		assertEquals(5, romNum.convertToInteger("V"));
+	}
+	
+	@Test public void testConvertToInt_Convert6toRoman() {
+		
+		assertEquals(6, romNum.convertToInteger("VI"));
+	}
+	
+	@Test public void testConvertToInt_Convert7toRoman() {
+		
+		assertEquals(7, romNum.convertToInteger("VII"));
+	}
+	
+	@Test public void testConvertToInt_Convert8toRoman() {
+		
+		assertEquals(8, romNum.convertToInteger("VIII"));
+	}
+	
+	@Test public void testConvertToInt_Convert9toRoman() {
+		
+		assertEquals(9, romNum.convertToInteger("IX"));
+	}
+	
+	@Test public void testConvertToInt_Convert10toRoman() {
+		
+		assertEquals(10, romNum.convertToInteger("X"));
+	}
+	
+	@Test public void testConvertToInt_Convert11toRoman() {
+		
+		assertEquals(11, romNum.convertToInteger("XI"));
+	}
+
+	@Test public void testConvertToInt_Convert12toRoman() {
+		
+		assertEquals(12, romNum.convertToInteger("XII"));
 	}
 
 }
